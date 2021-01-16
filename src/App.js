917e3a73a97504728f29d6fe0c537ad822c0dde6
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-01-15 19:56:03 -0800

    add words list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import WordsList from './components/WordList';
+import { draggableList } from './data/mock-data';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ol>
+          <WordsList wordsList={draggableList} />
+        </ol>
       </header>
     </div>
   );
---
+++ src/data/mock-data.jsx
@@ -0,0 +1,7 @@
+export const draggableList = [
+  { id: 'hi', word: 'Hi,' },
+  { id: 'i', word: 'I' },
+  { id: 'like', word: 'like' },
+  { id: 'coffee', word: 'coffee' },
+  { id: '!', word: '!' }
+];
