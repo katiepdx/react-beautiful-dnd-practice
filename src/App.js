251f684a56fce40014cec36a8426ce7c04186c8a
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-01-15 21:38:02 -0800

    add drag functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import './App.css';
-import WordsList from './components/WordList';
+// import WordsList from './components/WordList';
 import { draggableList } from './data/mock-data';
+import React from 'react'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <ol>
-          <WordsList wordsList={draggableList} />
-        </ol>
+        <DragDropContext>
+          <Droppable droppableId="words">
+            {(provided) => (
+              <div className="words" {...provided.droppableProps} ref={provided.innerRef}>
+                {draggableList.map((word, i) => {
+                  return (
+                    <Draggable draggableId={word.id} index={i} key={word.id}>
+                      {(provided) => (
+                        <div className="word-item" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                          Word: {word.word}
+                        </div>
+                      )}
+                    </Draggable>
+                  )
+                })}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </header>
     </div>
   );
