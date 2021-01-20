ca9e15de83f377b56d5cf3a25a9040746ae1e6ad
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-01-19 20:00:03 -0800

    refactor - add WordItem component

--- src/components/WordItem.jsx
+++ src/components/WordItem.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Draggable } from 'react-beautiful-dnd'
 
-const WordItem = props => {
+const WordItem = ({ word, index, currDragged }) => {
   return (
-    <div>
-      WordItem
-    </div>
+    <Draggable draggableId={word.id} index={index} key={word.id}>
+
+      {(provided) => (
+        <div className="word-item" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} id={index === currDragged ? 'dragging' : 'not-dragging'}>
+          {word.word}
+        </div>
+      )}
+    </Draggable>
   )
 }
 
 WordItem.propTypes = {
-
+  word: PropTypes.objectOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    word: PropTypes.string.isRequired
+  })),
+  index: PropTypes.number.isRequired,
 }
 
 export default WordItem
--- src/components/WordList.jsx
+++ src/components/WordList.jsx
@@ -1,30 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Draggable } from 'react-beautiful-dnd'
+import WordItem from './WordItem'
 
-const WordList = ({ wordList, currDragged }) => {
-  return (
-    wordList.map((word, index) => {
-      return (
-        <Draggable draggableId={word.id} index={index} key={word.id}>
-          {(provided) => (
-            <div className="word-item" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} id={index === currDragged ? 'dragging' : 'not-dragging'}>
-              {word.word}
-            </div>
-          )}
-
-        </Draggable>
-      )
-    })
-  )
-}
+const WordList = ({ wordList }) => (
+  wordList.map((word, index) => {
+    return (
+      <WordItem word={word} index={index} key={index} />
+    )
+  })
+)
 
 WordList.propTypes = {
   wordList: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
     word: PropTypes.string.isRequired
-  })),
-  currDragged: PropTypes.isRequired
+  }))
 }
 
 export default WordList
